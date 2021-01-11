336b8aa7cf7df448b4806b836539c7266063a099
MiltonDevSis <milton.dev.sis@gmail.com>
2021-01-11 11:29:07 -0300

    implementando com let

--- src/main/kotlin/br/com/teste/bytebank/collections/main.kt
+++ src/main/kotlin/br/com/teste/bytebank/collections/main.kt
@@ -1,18 +1,24 @@
 package br.com.teste.bytebank.collections
 
+import br.com.teste.bytebank.model.Endereco
+
 fun main() {
 
-    "".let(::testeRecebeString)
+//    val endereco = Endereco(logradouro = "Rua das flores", numero = 3185)
+//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    println(enderecoEmMaiusculo)
 
-    teste(1, {})
+    val endereEmMaiusculo = Endereco(logradouro = "Rua das flores", numero = 3185)
+        .let { endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+        }.let(::println)
 
+    listOf(
+        Endereco(complemento = "Casa"),
+        Endereco(),
+        Endereco(complemento = "Apt")
+    )
+        .filter { endereco -> endereco.complemento.isNotEmpty() }
+        .let(::println)
 }
 
-fun testeRecebeString(valor: String) {
-
-}
-
-// higher order function
-fun teste(teste: Int, bloco: () -> Unit) {
-
-}
--- src/main/kotlin/br/com/teste/bytebank/model/Endereco.kt
+++ src/main/kotlin/br/com/teste/bytebank/model/Endereco.kt
@@ -8,4 +8,14 @@ class Endereco(
     var estado: String = "",
     var cep: String = "",
     var complemento: String = ""
-)
+){
+    override fun toString(): String {
+        return "Endereco(logradouro='$logradouro', " +
+                "\nnumero=$numero, " +
+                "\nbairro='$bairro', " +
+                "\ncidade='$cidade', " +
+                "\nestado='$estado', " +
+                "\ncep='$cep', " +
+                "\ncomplemento='$complemento')"
+    }
+}
