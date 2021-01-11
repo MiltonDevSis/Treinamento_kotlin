2a7002160b090905bdb5b391abf6a41791686511
MiltonDevSis <milton.dev.sis@gmail.com>
2021-01-11 08:47:23 -0300

    Paradigma funcional

--- src/main/kotlin/br/com/teste/bytebank/collections/main.kt
+++ src/main/kotlin/br/com/teste/bytebank/collections/main.kt
@@ -2,8 +2,19 @@ package br.com.teste.bytebank.collections
 
 fun main() {
 
+    val minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao())
 
+    val minhaFuncaoClasse: () -> Unit = Teste()
+    println(minhaFuncaoClasse())
 
 }
+fun teste() {
+    println("Executa teste")
+}
 
-
+class Teste: () -> Unit{
+    override fun invoke() {
+        println("Executa invoke")
+    }
+}
